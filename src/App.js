6fe22b6648cfdb4a48b042a82a4aef6b2693d470
unknown <viratprasan14@gmail.com>
2020-12-16 19:52:31 +0530

    lorem ipsum generator

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import data from './data';
+function App() {
+
+  const [count,setCount] = useState(0)
+  const [text,setText] = useState([])
+
+
+  const handleSubmit = (e) => {
+       e.preventDefault()
+       let rakul = parseInt(count)
+
+        if(count <= 0) {
+          rakul = 1;
+        } else 
+        if(count > 200) {
+          rakul = 200;
+        }
+
+       console.log(typeof rakul)
+       setText(data.slice(0,rakul))
+  }
+ 
+  return (
+    <section className='section-center'>
+    <h3>tired of boring lorem ipsum?</h3>
+    <form className='lorem-form' onSubmit={handleSubmit}>
+      <label htmlFor='amount'>paragraphs:</label>
+      <input
+        type='number'
+        name='amount'
+        id='amount'
+        value={count}
+        onChange={(e) => setCount(e.target.value)}
+      />
+      <button className='btn'>generate</button>
+    </form>
+    <article className='lorem-text'>
+   {
+     text.map((item,index) => {
+       return <p key={index}>{item}</p>
+     })
+   }
+    </article>
+  </section>
+    )
+}
+
+export default App;
